a7e8125a8324f2c87243db5425cffb823a3763e8
Rafał Leszko <rafal@livepeer.org>
2022-10-07 21:20:52 -0500

    Add deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ETH_URL,
+      accounts: [process.env.STAGING_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
